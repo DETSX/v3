cfc0355b67d70e662a0a6e5744538913745ed737
DETSX <Dxtrshtt@gmail.com>
2026-05-28 16:31:43 +0800

    Update 875033288c5e99d576622aced60a3c44.lua

--- loaders/875033288c5e99d576622aced60a3c44.lua
+++ loaders/875033288c5e99d576622aced60a3c44.lua
@@ -1,1 +1,1 @@
-http://109.71.240.235:3910/api/run/eyJpZCI6IjRjYTNhNDdmLTZjZDEtNDUxZC1hYTk0LWMwMTJhYTQ0MjhkYSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjRjYTNhNDdmLTZjZDEtNDUxZC1hYTk0LWMwMTJhYTQ0MjhkYSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
